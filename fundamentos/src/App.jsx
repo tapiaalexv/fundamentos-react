cc6044f96179dfd5235f3701850a5064a677f834
Dario Tapia <alexis.tapia@epn.edu.ec>
2023-11-18 18:45:03 -0500

    Condiciones

--- fundamentos/src/App.jsx
+++ fundamentos/src/App.jsx
@@ -7,17 +7,38 @@ const MyButton = () => {
     )
 }
 
+const OnlineTxt = () => {
+    return (
+        <div>
+            <h1> Online </h1>
+        </div>
+    )
+}
+
+const OfflineTxt = () => {
+    return (
+        <div>
+            <h1> Offline </h1>
+        </div>
+    )
+}
+
 const App = () => {
 
     const title = "Mi titulo desde una constante";
     const classTitle = "text-center";
     const pathImg = "https://picsum.photos/200/300";
+    const user = true;
+    const user2 = true;
 
     return (
         <>
             <h1 className={classTitle}>{title.toUpperCase()}</h1>;
             <img src={pathImg} alt={`imagen-${title}`} />
             <MyButton/>
+            {user ? <OnlineTxt/> : <OfflineTxt/>}
+            {user2 && <OfflineTxt/>}
+
         </>
     );
 
